5ba310e1825e2e076c17dbab30833cf47d48e0d9
lin04869 <gaoxiaolin4069@163.com>
2025-12-23 21:28:32 +0800

    RDT3.0

--- TCP_Test_1/src/com/ouc/tcp/test/CheckSum.java
+++ TCP_Test_1/src/com/ouc/tcp/test/CheckSum.java
@@ -6,14 +6,19 @@ import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
 
 public class CheckSum {
-	
-	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
-	public static short computeChkSum(TCP_PACKET tcpPack) {
-		int checkSum = 0;
-		
-		
-		
-		return (short) checkSum;
-	}
-	
+
+    /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+    public static short computeChkSum(TCP_PACKET tcpPack) {
+//		int checkSum = 0;
+        CRC32 crc =new CRC32();
+        crc.update(tcpPack.getTcpH().getTh_ack());
+        crc.update(tcpPack.getTcpH().getTh_seq());
+        int data[];
+        data=tcpPack.getTcpS().getData();
+        for(int e:data){
+            crc.update(e);
+        }
+        return (short) (crc.getValue() & 0xffff); //取低16位
+    }
+
 }
